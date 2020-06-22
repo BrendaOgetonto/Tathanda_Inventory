56876caad99ab69b78e7840626aef2864271dfaa
brendaCodeFremics <brenda@codefremics.com>
2020-06-22 23:38:06 +0300

    first commit

---
+++ backend/database/migrations/2020_06_20_165550_create_employees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name')->nullable();
+            $table->string('phone')->unique()->nullable();
+            $table->string('id_number')->unique()->nullable();
+            $table->string('residence')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
